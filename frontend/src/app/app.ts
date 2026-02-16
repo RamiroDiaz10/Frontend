de6ce25351916d58a3ccee3682ab7ddc21212d78
Ramiro Diaz <jramiroda10@gmail.com>
2026-02-16 10:55:19 -0500

    [+]add pages components: home,cart,checkout,product-detail,products

--- frontend/src/app/app.ts
+++ frontend/src/app/app.ts
@@ -1,12 +1,23 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { Counter } from './features/components-counter/counter/counter';
+import { Cart } from './pages/cart/cart';
+import { Checkout } from './pages/checkout/checkout';
+import { Home } from './pages/home/home';
+import { ProductDetail } from './pages/product-detail/product-detail';
+import { Products } from './pages/products/products';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.html',
   styleUrl: './app.css',
-  imports: [RouterOutlet, Counter]
+  imports: [
+    RouterOutlet, 
+    Cart,
+    Checkout,
+    Home,
+    ProductDetail,
+    Products
+  ]
 })
 export class App {
   protected readonly title = signal('frontend');
