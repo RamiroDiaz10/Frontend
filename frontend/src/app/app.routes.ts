f861b00cfb55355e1b3b9f400b196f573cbf754e
Ramiro Diaz <jramiroda10@gmail.com>
2026-02-18 11:10:45 -0500

    [+]personalizar mensajes de error register

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -8,13 +8,34 @@ import { Error } from './pages/error/error';
 import { Register } from './pages/register/register';
 
 export const routes: Routes = [
-    { path: 'home', component: Home },
-    { path: 'products', component:Products},
-    { path: 'product-detail', component:ProductDetail},
-    { path: 'cart', component:Cart},
-    { path: 'checkout', component:Checkout},
-    { path: 'register', component:Register},
-    { path: '404', component:Error},
+    { 
+    path: 'home', 
+    component: Home
+    },
+    { 
+    path: 'products',
+    component:Products
+    },
+    {
+    path: 'product-detail',
+    component:ProductDetail
+    },
+    { 
+    path: 'cart',
+    component:Cart
+    },
+    { 
+    path: 'checkout',
+    component:Checkout
+    },
+    { 
+    path: 'register', 
+    component:Register
+    },
+    { 
+    path: '404', 
+    component:Error
+    },
     { path: '', redirectTo: 'home', pathMatch: 'full' },
     { path: '**', redirectTo: '404', pathMatch: 'full' }
 
--- frontend/src/app/pages/register/register.ts
+++ frontend/src/app/pages/register/register.ts
@@ -3,15 +3,15 @@ import { FormControl, FormGroup, ReactiveFormsModule, Validators } from'@angular
 
 @Component({
   selector: 'app-register',
-  imports: [ReactiveFormsModule],
+  imports: [ReactiveFormsModule ],
   templateUrl: './register.html',
-  styleUrl: './register.css',
+  styleUrl: './register.css', 
 })
 export class Register {
   formdata = new FormGroup({
-    name: new FormControl('' ,[Validators.required]),
+    name: new FormControl('' ,[Validators.required, Validators.minLength(3)]),
     email: new FormControl('',[Validators.required, Validators.email] ),
-    password: new FormControl('',[Validators.required, Validators.minLength(6)])
+    password: new FormControl('',[Validators.required, Validators.minLength(8)])
 
   });
 
