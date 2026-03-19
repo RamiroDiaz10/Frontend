ceafae8f76bf8b48a447bcb9848c5a08ed579937
Ramiro Diaz <jramiroda10@gmail.com>
2026-03-19 16:09:54 -0500

    [+]enviroments y config images

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: 'http://ec2-54-163-210-150.compute-1.amazonaws.com:3000/api/v1',
+  apiUrl: 'http://ec2-184-72-89-120.compute-1.amazonaws.com:3000/api/v1',
 };
